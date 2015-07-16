b63c1998dd0d5620b8a4b98281ac506818acbf55
ChristineSchatz <cmschatz@gmail.com>
2015-07-16 11:43:10 -0400

    Create schema and migrate

--- hacker_news/db/migrate/20150716152353_create_posts.rb
+++ hacker_news/db/migrate/20150716152353_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.string :title, null: false
+      t.string :body, null: false
+      t.integer :user_id, null: false
 
       t.timestamps null: false
     end
