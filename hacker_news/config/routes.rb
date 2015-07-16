4096e430c05ed73e2a4f0995b2c4e2242e9ede62
ChristineSchatz <cmschatz@gmail.com>
2015-07-16 12:00:58 -0400

    Create resources, get & post routes and welcome page

--- hacker_news/config/routes.rb
+++ hacker_news/config/routes.rb
@@ -3,7 +3,17 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+  resources :users
+
+  resources :posts do
+    resources :comments
+  end
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'signup' => 'users#new'
+  get 'logout' => 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
