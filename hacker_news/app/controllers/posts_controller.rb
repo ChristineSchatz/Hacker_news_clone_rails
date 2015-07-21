3fe9a4f839abab3af22e620c1d61aae9a5a8b9e0
Christine Schatz <cmschatz@gmail.com>
2015-07-21 00:01:51 -0400

    Finish user_posts controllers, new, edit, update, and destroy posts working

--- hacker_news/app/controllers/posts_controller.rb
+++ hacker_news/app/controllers/posts_controller.rb
@@ -3,20 +3,36 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def show
-    @post = Post.find_by(id: params[:id])
-    redirect_to post_path(@post)
+  def create
+    #you have to link the user to the post here
+    user = User.find_by_id(session[:user_id])
+    post = Post.new(post_params)
+    post.user = user
+    post.save
+    redirect_to :root
   end
 
-  def create
+  def show
+    @post = Post.find_by(id: params[:id])
+    @user = User.find_by(params[:user_id])
   end
 
   def edit
+    @post = Post.find_by(id: params[:id])
   end
 
   def update
+    Post.update(params[:id], post_params)
+    redirect_to user_path(session[:user_id])
   end
 
   def destroy
+    Post.find_by(id: params[:id]).destroy
+    redirect_to user_path(session[:user_id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id)
   end
 end
--- hacker_news/db/migrate/20150721015059_create_posts.rb
+++ hacker_news/db/migrate/20150721015059_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title, null: false
       t.string :body, null: false
-      t.integer :votes, null: false
+      t.integer :votes, default: 0
       t.integer :user_id, null: false
 
       t.timestamps null: false
