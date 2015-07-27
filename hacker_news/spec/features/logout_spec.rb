858ea8c03de1622912d01c28315deaae044fa7c0
Christine Schatz <cmschatz@gmail.com>
2015-07-27 17:47:45 -0400

    Fix logout spec

--- hacker_news/spec/features/logout_spec.rb
+++ hacker_news/spec/features/logout_spec.rb
@@ -8,17 +8,11 @@ feature "User logs out", :type => :feature do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(test_user)
   end
 
-  it "with username and password" do
+  it "and goes back to home page" do
     visit '/'
     fill_in("username", :with => 'Alina')
     fill_in("password", :with => '12345')
     click_button "Log In"
-    save_and_open_page
-    expect(page).to have_content('Logout')
-   end
-
-  it "and goes back to home page" do
-    visit root_path
     click_link 'Logout'
     save_and_open_page
     expect(current_path).to eq(root_path)
