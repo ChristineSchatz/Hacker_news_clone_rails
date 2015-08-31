6a025b829772fb261553dbbb6dcb7b51943a9098
Christine Schatz <cmschatz@gmail.com>
2015-08-30 20:11:22 -0400

    refactor comments create method and fix comment_params to merge user_id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,13 +10,11 @@ class CommentsController < ApplicationController
 
   def create
     post = Post.find(params[:post_id])
-    comment = Comment.new(comment_params)
-    comment.post_id = params[:post_id] #Remove line 12 / 14?
-    comment.user_id = session[:user_id]
+    comment = post.comments.build(comment_params)
     if comment.save && request.xhr?
       render json: {data: comment.body}.to_json
     else
-      redirect_to post, notice: {errors: "Invalid comment."}
+      redirect_to comment, notice: {errors: "Invalid comment."}
     end
   end
 
@@ -28,6 +26,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :post_id).merge(user_id: current_user.id)
   end
 end
