46c9d5e56a2d4b0380ebc61d825e6d3eef7c35f7
Christine Schatz <cmschatz@gmail.com>
2015-08-30 20:10:46 -0400

    change current user method and add is_authenticated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,19 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    User.find_by(id: session[:user_id])
+    @user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def permission_denied
     render :file => "public/401.html", :status => :unauthorized
   end
 
-   def require_login
+  def require_login
     redirect_to :root unless is_authenticated?
+  end
+
+  def is_authenticated?
+    !!current_user
   end
 
 end
