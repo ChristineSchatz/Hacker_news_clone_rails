54e55e2f67ccdccd5fb1c83438f5a9af5de6dd2f
Christine Schatz <cmschatz@gmail.com>
2015-09-04 13:12:52 -0400

    reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,17 @@ users = [
 ].each { |user| User.create(user) }
 
 posts = [
-   { title: "N8VEM – Homebrew Computing Project", body: "http://hermann.io/sofia.cremin", user_id: 6, votes: 0},
-   { title: "OS X Command Line Utilities", body: "http://hermann.io/sofia.cremin", user_id: 2, votes: 0},
-   { title: "The Web’s Cruft Problem", body: "http://hermann.io/sofia.cremin", user_id: 1, votes: 0},
-   { title: "Can we ever really know another person?", body: "http://hermann.io/sofia.cremin", user_id: 7, votes: 0},
-   { title: "Finnish Defence Forces Cyber Challenge", body: "http://hermann.io/sofia.cremin", user_id: 5, votes: 0},
-   { title: "How to learn data science", body: "http://hermann.io/sofia.cremin", user_id: 3, votes: 0},
-   { title: "The psychedelic state in light of integrated information theory", body: "http://hermann.io/sofia.cremin", user_id: 4, votes: 0},
-   { title: "Diet Advice That Ignores Hunger", body: "http://hermann.io/sofia.cremin", user_id: 1, votes: 0},
-   { title: "Estimate the cost of a Web, iOS or Android app", body: "http://hermann.io/sofia.cremin", user_id: 3, votes: 0},
-   { title: "Pyxley: Python Powered Dashboards", body: "http://hermann.io/sofia.cremin", user_id: 4, votes: 0},
-   { title: "RethinkDB 2.1 beta: automatic failover with Raft", body: "http://hermann.io/sofia.cremin", user_id: 5, votes: 0}
+   { title: "N8VEM – Homebrew Computing Project", body: "www.google.com", user_id: 6, votes: 0},
+   { title: "OS X Command Line Utilities", body: "www.google.com", user_id: 2, votes: 0},
+   { title: "The Web’s Cruft Problem", body: "www.google.com", user_id: 1, votes: 0},
+   { title: "Can we ever really know another person?", body: "www.google.com", user_id: 7, votes: 0},
+   { title: "Finnish Defence Forces Cyber Challenge", body: "www.google.com", user_id: 5, votes: 0},
+   { title: "How to learn data science", body: "www.google.com", user_id: 3, votes: 0},
+   { title: "The psychedelic state in light of integrated information theory", body: "www.google.com", user_id: 4, votes: 0},
+   { title: "Diet Advice That Ignores Hunger", body: "www.google.com", user_id: 1, votes: 0},
+   { title: "Estimate the cost of a Web, iOS or Android app", body: "www.google.com", user_id: 3, votes: 0},
+   { title: "Pyxley: Python Powered Dashboards", body: "www.google.com", user_id: 4, votes: 0},
+   { title: "RethinkDB 2.1 beta: automatic failover with Raft", body: "www.google.com", user_id: 5, votes: 0}
 ].each { |post| Post.create(post) }
+
 
